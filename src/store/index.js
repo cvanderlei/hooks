e1340318182587ff33f15eea7c85ed1c988ff643
Celso Vanderlei <celsovanderlei@gmail.com>
2021-02-20 11:32:12 -0300

    Until Feb, 20

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import {reducer} from "./reducers/index"
+//import {numberAdd2} from "./actions/number"
+
+const initialState = {
+    other: "...",
+    number: 0
+}
+
+export {
+    initialState,
+    reducer
+}
